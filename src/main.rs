e9e1b7ccf6746ff1019e1cd441d2893a02ddbeac
Albert Lanza <albert2000_lanza@hotmail.com>
2024-11-01 10:03:26 +0100

    Install dialoguer & first 2 questions (language & project)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use colored::*;
+use dialoguer::{theme::ColorfulTheme, Select};
 
 fn main() {
     println!("{}", "Spring Initializr CLI!".bright_green().bold());
@@ -6,5 +7,34 @@ fn main() {
         "{}",
         "https://github.com/AlbertLnz/spring-initializr-cli".bright_yellow()
     );
-    println!("{}", "Created by AlbertLnz".bright_cyan())
+    println!("{}", "Created by AlbertLnz".bright_cyan());
+
+    loop {
+        // LANGUAGE:
+        println!("{}", "Select the language:".bright_green());
+        let options = vec!["Java", "Kotlin", "Groovy"];
+
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .items(&options)
+            .default(0)
+            .interact()
+            .expect("Failed to read selection");
+
+        let language = options[selection];
+        println!(" {}\n", language);
+
+        // PROJECT
+        println!("{}", "Select the project:".bright_green());
+
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .items(&options)
+            .default(0)
+            .interact()
+            .expect("Failed to read selection");
+
+        let project = options[selection];
+        println!(" {}\n", project);
+
+        println!("You selected: {} - {}", language, project);
+    }
 }
